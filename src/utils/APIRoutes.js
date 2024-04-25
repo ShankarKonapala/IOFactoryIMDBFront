b38919f51b2634c53b3489e9718a36155ecc4494
ShankarKonapala <110994599+ShankarKonapala@users.noreply.github.com>
2024-04-25 11:59:19 +0530

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,4 @@
-export const host = "http://localhost:5000";
-// export const host = "https://iofactoryimdbback.onrender.com";
+export const host = "https://iofactoryimdbback.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const addMovieRoute = `${host}/api/movies/addMovie`;
